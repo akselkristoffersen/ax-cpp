7547ab31e22ecdc61228af106cae68e7f377521e
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-07-26 01:32:11 +0200

    Make packet_iterator

--- include/ax/packet_iterator.hpp
+++ include/ax/packet_iterator.hpp
@@ -1,10 +1,98 @@
-#include <iostream>
+#include <span>
+#include <iterator>
 
 namespace ax
 {
-    bool packet_iterator()
+    template<typename T, typename F>
+    class packet_iterator
     {
-        std::cout << "yoyoyo\n";
-        return true;
+    public:
+        packet_iterator() = default;
+        packet_iterator(std::span<T>, F);
+
+        using difference_type = std::ptrdiff_t;
+        using value_type = std::span<T>;
+
+        std::span<T> operator*() const;
+        packet_iterator& operator++();
+        packet_iterator operator++(int);
+        bool operator==(const packet_iterator&) const;
+        bool operator==(const std::default_sentinel_t&) const;
+
+        packet_iterator begin() const;
+        std::default_sentinel_t end() const;
+
+    private:
+        std::span<T> buffer;
+        std::size_t current_pos{};
+        F func;
+
+        [[nodiscard]] bool is_end() const
+        {
+            if (buffer.empty())
+            {
+                return true;
+            }
+            auto const next_size{ func(buffer.subspan(current_pos)) };
+            return next_size == 0 || current_pos + next_size > buffer.size();
+        }
+    };
+
+    template<typename T, typename F>
+    packet_iterator<T, F>::packet_iterator(std::span<T> buffer, F func)
+        : buffer(buffer), func(func)
+    {
+    }
+
+    template<typename T, typename F>
+    std::span<T> packet_iterator<T, F>::operator*() const
+    {
+        return buffer.subspan(current_pos, func(buffer.subspan(current_pos)));
+    }
+
+    template<typename T, typename F>
+    packet_iterator<T, F>& packet_iterator<T, F>::operator++()
+    {
+        current_pos += func(buffer.subspan(current_pos));
+        return *this;
+    }
+
+    template<typename T, typename F>
+    packet_iterator<T, F> packet_iterator<T, F>::operator++(int)
+    {
+        auto tmp{ *this };
+        ++*this;
+        return tmp;
+    }
+
+    template<typename T, typename F>
+    bool packet_iterator<T, F>::operator==(const packet_iterator& rhs) const
+    {
+        if (buffer.data() != rhs.buffer.data())
+        {
+            return false;
+        }
+        return current_pos == rhs.current_pos;
+    }
+
+    template<typename T, typename F>
+    bool packet_iterator<T, F>::operator==(const std::default_sentinel_t&) const
+    {
+        return is_end();
+    }
+
+    template<typename T, typename F>
+    packet_iterator<T, F> packet_iterator<T, F>::begin() const
+    {
+        return { buffer, func };
+    }
+
+    template<typename T, typename F>
+    std::default_sentinel_t packet_iterator<T, F>::end() const
+    {
+        return {};
     }
 }
+
+template<typename T, typename F>
+inline constexpr bool std::ranges::enable_borrowed_range<ax::packet_iterator<T, F>> = true;
--- test/test_packet_iterator.cpp
+++ test/test_packet_iterator.cpp
@@ -1,9 +1,63 @@
-#define CATCH_CONFIG_MAIN
 #include <catch2/catch_test_macros.hpp>
 #include <ax/packet_iterator.hpp>
+#include <ranges>
+#include <numeric>
 
-// Test cases
-TEST_CASE("test_packet_iterator", "[packet_iterator]")
+struct get_size
 {
-    REQUIRE(ax::packet_iterator());
+    std::size_t operator()(std::span<const std::byte> buf) const
+    {    
+        return std::to_integer<std::size_t>(buf.front());
+    }
+};
+
+TEST_CASE("is forward range")
+{
+    REQUIRE(std::ranges::forward_range<ax::packet_iterator<const std::byte, get_size>>);
+}
+
+TEST_CASE("default iterator")
+{
+    REQUIRE(std::ranges::distance(ax::packet_iterator<const std::byte, get_size>{}) == 0);
+}
+
+TEST_CASE("empty buffer")
+{
+    std::vector<int> const buffer{};
+    auto const buffer_as_bytes{ std::as_bytes(std::span(buffer)) };
+    REQUIRE(std::ranges::distance(ax::packet_iterator{buffer_as_bytes, get_size{}}) == 0);
+}
+
+TEST_CASE("complete packets")
+{
+    std::vector<std::uint8_t> const buffer{ 3, 0, 0, 2, 0, 4, 0, 0, 0 };
+    auto const buffer_as_bytes{ std::as_bytes(std::span(buffer)) };
+
+    SECTION("correct distance") 
+    {
+        REQUIRE(std::ranges::distance(ax::packet_iterator{buffer_as_bytes, get_size{}}) == 3);
+    }
+
+    SECTION("correct span sizes") 
+    {
+        auto it{ ax::packet_iterator{buffer_as_bytes, get_size{}} };
+        REQUIRE((*it).size() == 3);
+        REQUIRE((*++it).size() == 2);
+        REQUIRE((*++it).size() == 4);
+    }
+}
+
+TEST_CASE("incomplete packets 1")
+{
+    std::vector<std::uint8_t> const buffer{ 3, 0 };
+    auto const buffer_as_bytes{ std::as_bytes(std::span(buffer)) };
+    REQUIRE(std::ranges::distance(ax::packet_iterator{buffer_as_bytes, get_size{}}) == 0);
+}
+
+TEST_CASE("incomplete packets 2")
+{
+    std::vector<std::uint8_t> const buffer{ 3, 0, 0, 2 };
+    auto const buffer_as_bytes{ std::as_bytes(std::span(buffer)) };
+    REQUIRE(std::ranges::distance(ax::packet_iterator{buffer_as_bytes, get_size{}}) == 1);
+    REQUIRE((*ax::packet_iterator{buffer_as_bytes, get_size{}}).size() == 3);
 }
